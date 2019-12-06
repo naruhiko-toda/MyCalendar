f0724954d075b0f8b5479303cb771fda582d444c
Naruhiko Toda <nr27bypphy@gmail.com>
2019-12-06 23:24:57 +0900

    viewの作成,pathの設定,adminの設定

--- calen/admin.py
+++ calen/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import User, Action, Schedule, Condition
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Action)
+admin.site.register(Schedule)
+admin.site.register(Condition)

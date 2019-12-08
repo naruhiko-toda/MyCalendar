01bdc6fece93072f0e7536b7e1a7695d0c5da6a5
Naruhiko Toda <nr27bypphy@gmail.com>
2019-12-08 16:11:33 +0900

    envファイルの作成

--- mycalendar/settings.py
+++ mycalendar/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import environ
+
+env = os.environ.get('DJANGO_ENV')
+environ.Env.read_env('./.env')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -119,4 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [
+    os.environ['DJANGO_PATH']+'static/',
+]

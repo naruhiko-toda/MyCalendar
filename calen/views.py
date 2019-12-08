a81f406057bbde6672edfc3ab1fc831d75a1af28
Naruhiko Toda <nr27bypphy@gmail.com>
2019-12-08 16:36:52 +0900

    calenderの導入 & temoplate moduleの導入

--- calen/views.py
+++ calen/views.py
@@ -1,4 +1,9 @@
+import json
+import calendar
+
 from django.shortcuts import render
+from django.http.response import JsonResponse
 
 def index(request):
-    return render(request, 'calen/index.html', {})
+    res = {}
+    return render(request, "calen/index.html", res)

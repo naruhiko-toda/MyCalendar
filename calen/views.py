646a30fca6af0511cf702f8295e54581feb31500
Naruhiko Toda <nr27bypphy@gmail.com>
2019-12-08 18:10:50 +0900

    月カレンダーを表示できるようにした

--- calen/views.py
+++ calen/views.py
@@ -1,9 +1,17 @@
+import os
 import json
 import calendar
+import datetime
 
 from django.shortcuts import render
 from django.http.response import JsonResponse
 
 def index(request):
     res = {}
+    now = datetime.datetime.now()
+    #デフォルトで今月の月間カレンダーを表示する
+    calendar.setfirstweekday(6)
+    default_calendar = calendar.monthcalendar(now.year, now.month)
+    res["calendar"] = default_calendar
+    print(default_calendar)
     return render(request, "calen/index.html", res)

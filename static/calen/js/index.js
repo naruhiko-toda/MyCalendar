645b0fc2a9234d374d17ccbda58f843162f95dcc
Naruhiko Toda <nr27bypphy@gmail.com>
2019-12-09 00:04:14 +0900

    日付にクラスを振った / 表示月を表示

--- static/calen/js/index.js
+++ static/calen/js/index.js
@@ -3,9 +3,24 @@ function create_calendar(){
     case "month":
       for(var i=0; i<calendar["value"].length; i++){
         $(".calendar_month tbody").append("<tr></tr>")
+        $(".calendar_month tbody").append("<tr></tr>")
         for(var j=0; j<7; j++){
-          $(".calendar_month tbody > tr:nth-child("+parseInt(i+1)+")").append("<td>"+calendar["value"][i][j]+"</td>")
+          if(calendar["value"][i][j] != 0){
+            $(".calendar_month tbody > tr:nth-child("+parseInt(2*i+1)+")").append("<td class='calen_top' id="+thisYear+"_"+thisMonth+"_"+calendar["value"][i][j]+">"+calendar["value"][i][j]+"</td>")
+            $(".calendar_month tbody > tr:nth-child("+parseInt(2*i)+")").append("<td class='clean_bottom'></td>")
+          }else{
+            $(".calendar_month tbody > tr:nth-child("+parseInt(2*i+1)+")").append("<td class='calen_top'></td>")
+            $(".calendar_month tbody > tr:nth-child("+parseInt(2*i)+")").append("<td class='clean_bottom'></td>")
+          }
         }
+      }
+      break;
+    case "week":
+      $(".calendar_week tbody").append("<tr></tr>")
+      $(".calendar_week tbody").append("<tr></tr>")
+      for(var j=0; j<7; j++){
+        $(".calendar_month tbody > tr:nth-child("+parseInt(2*i+1)+")").append("<td>"+calendar["value"][i]+"</td>")
+        $(".calendar_month tbody > tr:nth-child("+parseInt(2*i)+")").append("<td></td>")
       }
       break;
   }

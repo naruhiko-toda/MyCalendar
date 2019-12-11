f1ca8b0831145559502bedb586dd6d27dd309001
Naruhiko Toda <nr27bypphy@gmail.com>
2019-12-11 22:35:30 +0900

    新規作成用のmodal作成

--- static/calen/js/index.js
+++ static/calen/js/index.js
@@ -15,7 +15,7 @@ function create_calendar(){
             $(".calendar_month tbody > tr:nth-child("+parseInt(2*i+2)+")").append("<td class='calen_bottom'></td>")
           }
         }
-        bottom_height = ($(window).height()-26-56-(27*calendar["value"].length)) / calendar["value"].length
+        bottom_height = ($(window).height()-26-56-56-(27*calendar["value"].length)) / calendar["value"].length
         $(".calen_bottom").css("height",bottom_height)
       }
       break;
@@ -30,8 +30,8 @@ function create_calendar(){
   }
 }
 
-function display_month(month){
-  $(".thisMonth").html(month+" 月")
+function display_year_month(year,month){
+  $(".this_year_month").html(year+"年"+month+" 月")
 }
 
 function draw_today(this_year,this_month,this_date){
@@ -59,8 +59,9 @@ function calendar_move(element){
         calendar = JSON.parse(data["calendar"])
         console.log(display_time)
         console.log(calendar)
-        display_month(display_time["display_month"])
+        display_year_month(display_time["display_year"],display_time["display_month"])
         create_calendar();
+        draw_today(this_year,this_month,this_date);
       },
       function () {
         alert("読み込み失敗");
@@ -78,3 +79,14 @@ function create_schedule(element){
   console.log(selected_month)
   console.log(selected_date)
 }
+
+
+$('#exampleModal').on('show.bs.modal', function (event) {
+  var button = $(event.relatedTarget) // Button that triggered the modal
+  var recipient = button.data('whatever') // Extract info from data-* attributes
+  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+  var modal = $(this)
+  modal.find('.modal-title').text('New message to ' + recipient)
+  modal.find('.modal-body input').val(recipient)
+})

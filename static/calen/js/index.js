0db1bcb8723aff97a1ead1f43af37e77c1aa87ec
Naruhiko Toda <nr27bypphy@gmail.com>
2019-12-14 13:44:12 +0900

    画面に入ってログインmodalが出るようにした

--- static/calen/js/index.js
+++ static/calen/js/index.js
@@ -8,14 +8,26 @@ function create_calendar(){
         for(var j=0; j<7; j++){
           console.log(j)
           if(calendar["value"][i][j] != 0){
-            $(".calendar_month tbody > tr:nth-child("+parseInt(2*i+1)+")").append("<td class='calen_top' id="+display_time["display_year"]+"_"+display_time["display_month"]+"_"+calendar["value"][i][j]+">"+calendar["value"][i][j]+"</td>")
-            $(".calendar_month tbody > tr:nth-child("+parseInt(2*i+2)+")").append("<td class='calen_bottom' id="+display_time["display_year"]+"_"+display_time["display_month"]+"_"+calendar["value"][i][j]+" onclick='create_schedule(this);'></td>")
+            $(".calendar_month tbody > tr:nth-child("+parseInt(2*i+1)+")").append("\
+              <td class='calen_top'\
+              id="+display_time["display_year"]+"_"+display_time["display_month"]+"_"+calendar["value"][i][j]+">\
+              "+calendar["value"][i][j]+"</td>"
+            )
+            $(".calendar_month tbody > tr:nth-child("+parseInt(2*i+2)+")").append("\
+              <td class='calen_bottom'\
+              id="+display_time["display_year"]+"_"+display_time["display_month"]+"_"+calendar["value"][i][j]+"\
+              onclick='create_schedule(this);'></td>"
+            )
           }else{
-            $(".calendar_month tbody > tr:nth-child("+parseInt(2*i+1)+")").append("<td class='calen_top'></td>")
-            $(".calendar_month tbody > tr:nth-child("+parseInt(2*i+2)+")").append("<td class='calen_bottom'></td>")
+            $(".calendar_month tbody > tr:nth-child("+parseInt(2*i+1)+")").append("\
+              <td class='calen_top'></td>"
+            )
+            $(".calendar_month tbody > tr:nth-child("+parseInt(2*i+2)+")").append("\
+              <td class='calen_bottom'></td>"
+            )
           }
         }
-        bottom_height = ($(window).height()-26-56-56-(27*calendar["value"].length)) / calendar["value"].length
+        bottom_height = ($(window).height()-138-(27*week_cnt)) / week_cnt
         $(".calen_bottom").css("height",bottom_height)
       }
       break;
@@ -81,17 +93,15 @@ function create_schedule(element){
 }
 
 
-$('#exampleModal').on('show.bs.modal', function (event) {
+$('#schedule_modal').on('show.bs.modal', function (event) {
   var button = $(event.relatedTarget) // Button that triggered the modal
   var recipient = button.data('whatever') // Extract info from data-* attributes
-  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
   var modal = $(this)
   modal.find('.modal-title').text('New message to ' + recipient)
   modal.find('.modal-body input').val(recipient)
 })
 
-$(function() {
+function display_time_picker(){
   $(".start_date").datepicker();
   console.log(this_hour)
   if(this_hour==23){
@@ -110,59 +120,77 @@ $(function() {
     $(".finish_date").val(this_year+'/'+this_month+'/'+this_date);
   }
   $('.start_time').timepicker({
-    timeFormat: 'H:mm',
-    interval: 60,
-    minTime: '0:00',
-    maxTime: '23:00',
-    defaultTime: start_default+":00",
-    startTime: '0:00',
-    dynamic: false,
-    dropdown: true,
-    scrollbar: true,
-    zindex: 1100
-    });
-  $('.finish_time').timepicker({
-    timeFormat: 'H:mm',
-    interval: 60,
-    minTime: '0:00',
-    maxTime: '23:00',
-    defaultTime: finish_default+":00",
-    startTime: '0:00',
-    dynamic: false,
-    dropdown: true,
-    scrollbar: true,
-    zindex: 1100
-    });
+    timeFormat    : 'H:mm',
+    interval      : 60,
+    minTime       : '0:00',
+    maxTime       : '23:00',
+    defaultTime   : start_default+":00",
+    startTime     : '0:00',
+    dynamic       : false,
+    dropdown      : true,
+    scrollbar     : true,
+    zindex        : 1100
   });
+  $('.finish_time').timepicker({
+    timeFormat    : 'H:mm',
+    interval      : 60,
+    minTime       : '0:00',
+    maxTime       : '23:00',
+    defaultTime   : finish_default+":00",
+    startTime     : '0:00',
+    dynamic       : false,
+    dropdown      : true,
+    scrollbar     : true,
+    zindex        : 1100
+  });
+}
 
-  function check_time(){
-    start_date_list = $(".start_date").val().split("/")
-    start_year = start_date_list[0]
-    start_month = start_date_list[1]
-    start_date = start_date_list[2]
-    start_time_list = $(".start_time").val().split(":")
-    start_hour = start_time_list[0]
-    start_minute = start_time_list[1]
-    start = new　Date(start_year,start_month,start_date,start_hour,start_minute,0)
-    finish_date_list = $(".finish_date").val().split("/")
-    finish_year = finish_date_list[0]
-    finish_month = finish_date_list[1]
-    finish_date = finish_date_list[2]
-    finish_time_list = $(".finish_time").val().split(":")
-    finish_hour = finish_time_list[0]
-    finish_minute = finish_time_list[1]
-    finish = new　Date(finish_year,finish_month,finish_date,finish_hour,finish_minute,0)
-    console.log(start);
-    console.log(finish);
-    console.log()
-    if(start > finish){
-      $("#register_schedule_button").prop("disabled", true);
-      $(".finish_date").addClass("error_input")
-      $(".finish_time").addClass("error_input")
-      alert("不適切な時間です。開始時間より終了時間が後になるようにしてください！")
-    }else{
-      $("#register_schedule_button").prop("disabled", false);
-      $(".finish_date").removeClass("error_input")
-      $(".finish_time").removeClass("error_input")
-    }
+function check_time(){
+  // 予定が始まる日付
+  start_date_list   = $(".start_date").val().split("/")
+  start_year 　　　  = start_date_list[0]
+  start_month       = start_date_list[1]
+  start_date        = start_date_list[2]
+
+  // 予定が始まる時間
+  start_time_list   = $(".start_time").val().split(":")
+  start_hour        = start_time_list[0]
+  start_minute      = start_time_list[1]
+
+  start             = new　Date(start_year,start_month,start_date,start_hour,start_minute,0)
+
+  // 予定が終わる日付
+  finish_date_list  = $(".finish_date").val().split("/")
+  finish_year       = finish_date_list[0]
+  finish_month      = finish_date_list[1]
+  finish_date       = finish_date_list[2]
+
+  // 予定が終わる時間
+  finish_time_list  = $(".finish_time").val().split(":")
+  finish_hour       = finish_time_list[0]
+  finish_minute     = finish_time_list[1]
+
+  finish            = new　Date(finish_year,finish_month,finish_date,finish_hour,finish_minute,0)
+  console.log(start);
+  console.log(finish);
+  if(start > finish){
+    $("#register_schedule_button").prop("disabled", true);
+    $(".finish_date").addClass("error_input")
+    $(".finish_time").addClass("error_input")
+    alert("不適切な時間です。開始時間より終了時間が後になるようにしてください！")
+  }else{
+    $("#register_schedule_button").prop("disabled", false);
+    $(".finish_date").removeClass("error_input")
+    $(".finish_time").removeClass("error_input")
   }
+}
+
+function check_user_login_statement(){
+  // if user is not login
+  $('.js-modal').fadeIn();
+  $('.js-modal-close').on('click',function(){
+    $('.js-modal').fadeOut();
+  // if user is login
+    return false;
+  });
+}

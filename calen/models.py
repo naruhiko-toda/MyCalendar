e94b72cb9b980db7b1042c340ea26bef7bbbf229
Naruhiko Toda <nr27bypphy@gmail.com>
2019-12-14 18:42:56 +0900

    schedule作成のview構築

--- calen/models.py
+++ calen/models.py
@@ -5,11 +5,6 @@ from django.utils import timezone
 from django.contrib.postgres.fields import JSONField
 from django.contrib.auth.models import User
 
-
-GENDER_CHOICES = (
-(1, '男性'),
-(2, '女性'),
-)
 SLEEP_FEEDBACK = (
 (1, 'Excellent'),
 (2, 'Great'),
@@ -19,28 +14,26 @@ SLEEP_FEEDBACK = (
 )
 
 class Action(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    action = JSONField()
-    created_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)
+    user_id     = models.ForeignKey(User, on_delete = models.CASCADE)
+    action      = JSONField()
+    created_at  = models.DateTimeField(default = timezone.now)
 
 class Categories(models.Model):
-    category = models.CharField(max_length=50)
-    created_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)
+    user_id     = models.ForeignKey(User, on_delete = models.CASCADE)
+    category    = models.CharField(max_length = 50, unique = True)
+    created_at  = models.DateTimeField(default = timezone.now)
 
 class Schedule(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    category_id = models.ForeignKey(Categories, on_delete=models.CASCADE)
-    title = models.CharField('概要',max_length=20)
-    description = models.TextField('詳細な説明', blank=True)
-    start_time = models.TimeField('開始時間', default=datetime.time(7, 0, 0))
-    end_time = models.TimeField('終了時間', default=datetime.time(7, 0, 0))
-    date = models.DateField('日付', default=timezone.now)
-    created_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)
+    user_id     = models.ForeignKey(User, on_delete = models.CASCADE)
+    category_id = models.ForeignKey(Categories, on_delete = models.CASCADE)
+    title       = models.CharField('概要',max_length = 20)
+    description = models.TextField('詳細な説明', blank = True)
+    start_time  = models.TimeField('開始時間', default = datetime.time(7, 0, 0))
+    finish_time = models.TimeField('終了時間', default = datetime.time(7, 0, 0))
+    created_at  = models.DateTimeField(default = timezone.now)
 
 class Condition(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    date = models.DateTimeField()
-    condition = models.IntegerField(choices=SLEEP_FEEDBACK)
+    user_id     = models.ForeignKey(User, on_delete = models.CASCADE)
+    date        = models.DateTimeField()
+    condition   = models.IntegerField(choices = SLEEP_FEEDBACK)
+    created_at  = models.DateTimeField(default = timezone.now)
--- calen/views.py
+++ calen/views.py
@@ -7,80 +7,118 @@ from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
 from django.http.response import JsonResponse
 from django.core import serializers
-
-from django.contrib.auth.models import User
-from django.contrib.auth import login, authenticate
 from django.views.generic import CreateView
-from . forms import UserCreateForm, LoginForm
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import User
+
+from .forms import UserCreateForm, LoginForm
+from .models import Action, Schedule, Condition, Categories
 
 calendar.setfirstweekday(6)
 
 def index(request):
     res = {}
+
     if request.GET.get('username'):
         res["user"] = json.dumps({
-            "login" : True,
-            "username" : str(request.GET.get('username'))
+            "login"     : True,
+            "username"  : str(request.GET.get('username'))
         })
     else:
         res["user"] = json.dumps({
-            "login" : False,
-            "username" : None
+            "login"     : False,
+            "username"  : None
         })
+
     now = datetime.datetime.now()
     #デフォルトで今月の月間カレンダーを表示する
     default_calendar = calendar.monthcalendar(now.year, now.month)
     res["calendar"] = json.dumps({
-        "value": default_calendar,
-        "type": "month"
+        "value" : default_calendar,
+        "type"  : "month"
     })
+
     res["display_time"] = json.dumps({
-        "display_date":str(now.day),
-        "display_year":str(now.year),
-        "display_month":str(now.month),
+        "display_date"  : str(now.day),
+        "display_year"  : str(now.year),
+        "display_month" : str(now.month),
     })
+
     res["sign_in_form"] = LoginForm()
     res["sign_up_form"] = UserCreateForm()
+
+    caregories = []
+    print(Categories.objects.all())
+    print(res)
     return render(request, "calen/index.html", res)
+
+@csrf_exempt
+def create_shedule(request):
+    res = {}
+    user_id     = User.objects.get(username = request.POST.get('user')).id
+    category    = request.POST.get('category')
+    title       = request.POST.get('title')
+    description = request.POST.get('description')
+    start_time  = request.POST.get('start_time')
+    finish_time = request.POST.get('finish_time')
+
+    print(Categories.objects.filter(
+        category = category,
+        user_id  = user_id
+    ))
+    if Categories.objects.filter(
+        category = category,
+        user_id  = user_id
+    ):
+        Categories.objects.create(category = category)
+
+    Schedule.objects.create(
+        user_id     = user_id,
+        category    = category,
+        title       = title,
+        description = description,
+        start_time  = start_time,
+        finish_time = finish_time
+    )
+    return JsonResponse(res)
 
 @csrf_exempt
 def get_calendar(request):
     res={}
-    display_year = int(request.POST["display_year"])
-    display_month = int(request.POST["display_month"])
-    display_date = int(request.POST["display_date"])
+    display_year    = int(request.POST["display_year"])
+    display_month   = int(request.POST["display_month"])
+    display_date    = int(request.POST["display_date"])
     if request.POST["type"]=="forward":
         tar_calendar,tar_year,tar_month = get_forward_calendar(display_year, display_month)
     else:
         tar_calendar,tar_year,tar_month = get_before_calendar(display_year, display_month)
     res["calendar"] = json.dumps({
-        "value": tar_calendar,
-        "type": "month"
+        "value" : tar_calendar,
+        "type"  : "month"
     })
     res["display_time"] = json.dumps({
-        "display_date":str(display_date),
-        "display_year":str(tar_year),
-        "display_month":str(tar_month),
+        "display_date"  : str(display_date),
+        "display_year"  : str(tar_year),
+        "display_month" : str(tar_month),
     })
     return JsonResponse(res)
 
-
 def get_before_calendar(year, month):
     if month == 1:
-        tar_year = year - 1
-        tar_month = 12
+        tar_year    = year - 1
+        tar_month   = 12
     else:
-        tar_year = year
-        tar_month = month - 1
+        tar_year    = year
+        tar_month   = month - 1
     return calendar.monthcalendar(tar_year, tar_month),tar_year,tar_month
 
 def get_forward_calendar(year, month):
     if month == 12:
-        tar_year = year + 1
-        tar_month = 1
+        tar_year    = year + 1
+        tar_month   = 1
     else:
-        tar_year = year
-        tar_month = month + 1
+        tar_year    = year
+        tar_month   = month + 1
     return calendar.monthcalendar(tar_year, tar_month),tar_year,tar_month
 
 @csrf_exempt
@@ -91,9 +129,7 @@ def sign_up(request):
     if form.is_valid():
         form.save()
         username = form.cleaned_data.get('username')
-        print(username)
         password = form.cleaned_data.get('password1')
-        print(password)
         user = authenticate(username=username, password=password)
         login(request, user)
         user = str(user)
